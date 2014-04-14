6355236757fa0d05004bf965605e8c7d2ee1ecdb
Sandtears <me@sandtears.com>
2014-04-14 13:45:46 +0800

    加入 getAllFunctions 方法。
    /** getAllFunctions
    * 获取某个 java 文件中所有的函数名
    * @param pathname java 文件的路径
    * @return 所有的函数名的数组，类型为 ArrayList<String>
    */

--- src/com/aspectj/analysis/AnalysisTool.java
+++ src/com/aspectj/analysis/AnalysisTool.java
@@ -3,11 +3,17 @@ package com.aspectj.analysis;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.Stack;
+import java.util.regex.*;
 
 import com.aspectj.demo.Frame;
 import com.aspectj.tree.xmlResultTreeNode;
 
 public class AnalysisTool {
+    /** copyFile
+     *  复制文件
+     * @param source 源文件路径（包含文件名）
+     * @param target 目标文件路径（包含文件名）
+     */
 	public static void copyFile(File source, File target) {	
 		InputStream fis = null;
         OutputStream fos = null;
@@ -26,20 +32,24 @@ public class AnalysisTool {
 			try {
 				fis.close();
 			} catch (IOException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
             try {
 				fos.close();
 			} catch (IOException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
         }
 		return;
 	}
 	
-	public static void runAnalysis(String filepath, String filename, String ajFileName) {
+	/** _runAnalysis
+	 * 内部函数，运行分析功能
+	 * @param filepath 要分析的文件地址
+	 * @param filename 要分析的文件名
+	 * @param ajFileName 使用的 aj 文件
+	 */
+	public static void _runAnalysis(String filepath, String filename, String ajFileName) {
 		
 		System.setProperty("user.dir", filepath);
 		
@@ -48,14 +58,12 @@ public class AnalysisTool {
 			Runtime.getRuntime().exec(command, null, new File(filepath));
 			System.out.println(command);
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
 		try {
 			Thread.sleep(3000);
 		} catch (InterruptedException e1) {
-			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
 
@@ -65,11 +73,14 @@ public class AnalysisTool {
 		try {
 			Runtime.getRuntime().exec(command, null, new File(filepath));
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
 	
+	/** analysis
+	 * 分析用函数
+	 * @param pathname 要分析的目标文件
+	 */
 	public static void analysis(String pathname) {
 		
 		// Get filepath & filename
@@ -83,13 +94,13 @@ public class AnalysisTool {
 		// copy analysis.aj to ajFileName
 		copyFile(new File("static/analysis.aj"), new File(filepath + "\\" + ajFileName));
 		
-		// 删除原本的 XML 文件
+		// 判断 XML 是否存在，如果存在则删除。
 		File xmlResultFile = new File(filepath + "\\analysisResult.xml");
 		if (xmlResultFile.exists()) {
 			xmlResultFile.delete();
 		}
 		
-		// 添加 XML 文件头
+		// 生成  XML 文件
 		FileWriter fw;
 		try {
 			fw = new FileWriter(xmlResultFile);
@@ -97,16 +108,22 @@ public class AnalysisTool {
 			fw.write(xmlHeaderString);
 			fw.close();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
 		
 		// Run analysis
-		runAnalysis(filepath, filename, ajFileName);
+		_runAnalysis(filepath, filename, ajFileName);
 	}
 	
+	/** analysisXMLFile
+	 * 分析 XML 文件并返回结果集，其中第一个 xmlResultTreeNode 是根节点
+	 * @param filepath xml 文件的地址
+	 * @return 返回结果集，其中第一个 xmlResultTreeNode 是根节点
+	 * @throws IOException 文件不存在则报错
+	 */
 	public static ArrayList<xmlResultTreeNode> analysisXMLFile(String filepath) throws IOException {
+
 
 		BufferedReader bReader = new BufferedReader(new FileReader(filepath));
 		
@@ -128,7 +145,48 @@ public class AnalysisTool {
 			}
 			nodeContent = bReader.readLine();
 		}
+		bReader.close();
+		return resultNodes;  
+	}
+
+	/** getAllFunctions
+	 * 获取某个 java 文件中所有的函数名
+	 * @param pathname java 文件的路径
+	 * @return 所有的函数名的数组，类型为 ArrayList<String>
+	 */
+	public static ArrayList<String> getAllFunctions(String pathname)
+	{
+		// 编写正则表达式并编译
+		String regex = "(public|private|protected)+\\s*(static)?\\s*\\S+\\s*\\S+\\s*\\(.*?\\)";
+		Pattern re = Pattern.compile(regex);
 		
-		return resultNodes;  // 返回所有节点，其中第一个为根节点。
+		// 读取 java 文件内容
+		BufferedReader bReader = null;
+		try {
+			bReader = new BufferedReader(new FileReader(pathname));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		String fileContent = "";
+		String tmpString = "";
+		try {
+			do {
+				tmpString = bReader.readLine();
+				fileContent += tmpString;
+			} while (tmpString != null);
+			bReader.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		ArrayList<String> result = new ArrayList<String>();
+		
+		// 将匹配方法加入 ArrayList
+		Matcher matcher = re.matcher(fileContent);
+		while (matcher.find()) {
+			result.add(matcher.group());
+		}
+		// 返回结果
+		return result;
 	}
 }
